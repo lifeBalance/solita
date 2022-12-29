8e996f9d9aaf6100e4ef9ad45ed421959b83027f
javi <3051496+lifeBalance@users.noreply.github.com>
2022-12-29 12:21:43 +0200

    add React + tailwind + react router

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import reactLogo from './assets/react.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="text-4xl">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
