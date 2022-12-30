95846aa6fda6f5f588e6ce306f4c703e67c1ba47
javi <3051496+lifeBalance@users.noreply.github.com>
2022-12-30 14:19:11 +0200

    fixed distance in Km. in JourneyCard

--- client/src/components/JourneyCard.jsx
+++ client/src/components/JourneyCard.jsx
@@ -12,7 +12,7 @@ function JourneyCard(props) {
       </p>
 
       <p>
-        <span className=''>Distance:</span> <span className='font-bold'>{journey.distance}</span> meters
+        <span className=''>Distance:</span> <span className='font-bold'>{parseFloat((journey.distance) / 1000).toFixed(3)}</span> Km.
       </p>
 
       <p>
